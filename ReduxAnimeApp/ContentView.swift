0308d1a03fd86770f9d01add3abc76631655ed75
cw-orita <orita@chatwork.com>
2021-05-03 15:18:41 +0900

    first commit

---
+++ ReduxAnimeApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ReduxAnimeApp
+//
+//  Created by cw-orita on 2021/05/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ReduxAnimeApp/ReduxAnimeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReduxAnimeAppApp.swift
+//  ReduxAnimeApp
+//
+//  Created by cw-orita on 2021/05/03.
+//
+
+import SwiftUI
+
+@main
+struct ReduxAnimeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
