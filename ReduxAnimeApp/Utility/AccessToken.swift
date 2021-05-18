bbf7a4c70b2f19b542bcaf0633f130abd9c0fee8
cw-orita <orita@chatwork.com>
2021-05-18 12:01:58 +0900

    AccessToken.swift を追加

---
+++ ReduxAnimeApp/Utility/AccessToken.swift
@@ -0,0 +1,12 @@
+//
+//  AccessToken.swift
+//  ReduxAnimeApp
+//
+//  Created by cw-orita on 2021/05/03.
+//
+
+import Foundation
+
+struct AccessToken {
+    static let annict = "[ここに自分のアクセストークンを入れてください]"
+}
